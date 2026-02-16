e854a8ae2de85be2b44504a953a66dd12ff1a732
leo-cabezas <leocabezas@protonmail.com>
2026-02-16 16:26:04 -0600

    Basic library changes

---
+++ src/run/radio-rx-task.cpp
@@ -0,0 +1,36 @@
+#include "../lib/radio_lib.h"
+
+void radioRX_setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+  while (!Serial) delay(1);
+  delay(100);
+
+  Serial.println("Feather LoRa RX Test!");
+  
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  rf95.setTxPower(RFM95_POWR, false);
+}
+
+void radioRX_loop(){
+  
+}
