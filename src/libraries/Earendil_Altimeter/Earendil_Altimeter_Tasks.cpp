84577bd09151ed9812a27df122ba83463d3f9afc
leo-cabezas <leocabezas@protonmail.com>
2026-04-29 15:08:52 -0500

    Solved incoming change again

--- src/libraries/Earendil_Altimeter/Earendil_Altimeter_Tasks.cpp
+++ src/libraries/Earendil_Altimeter/Earendil_Altimeter_Tasks.cpp
@@ -42,44 +42,6 @@ namespace Earendil_Altimeter {
     void vAltimeter(void* pvParameters){
         (void)pvParameters;     // Parameters unused.
         
-<<<<<<< HEAD
-        if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-            printf("Could not find a valid BMP3 sensor, check wiring!");
-            vTaskDelete(NULL);
-        }
-
-        // Set up oversampling and filter initialization
-        bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_2X);
-        bmp.setPressureOversampling(BMP3_OVERSAMPLING_16X);
-        bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_7);
-        bmp.setOutputDataRate(BMP3_ODR_1_5_HZ);
-
-        SensorData_t temperature;
-        temperature.eSource = altTemp;
-        SensorData_t pressure;
-        pressure.eSource = altPress;
-
-        while(1){
-            if (!bmp.performReading()) {
-                printf("Failed to perform reading :(");
-                vTaskDelete(NULL);
-            }
-
-            temperature.eData = bmp.temperature;
-            pressure.eData = bmp.pressure;
-
-            if (xQueueSend(auQueue, &temperature, 0) != pdPASS){
-                printf("Failed to send temperature.");
-                vTaskDelete(NULL);
-            }
-            if (xQueueSend(auQueue, &pressure, 0) != pdPASS){
-                printf("Failed to send pressure.");
-                vTaskDelete(NULL);
-            }
-
-            vTaskDelay(pdMS_TO_TICKS(5000));
-        }
-=======
         vTaskDelay(pdMS_TO_TICKS(10000));
           
           while (1){
@@ -89,6 +51,5 @@ namespace Earendil_Altimeter {
 
             vTaskDelay(pdMS_TO_TICKS(5000));  
           }
->>>>>>> 57615aab13e960a6adf6554990b17741fa58444b
     }
 }
